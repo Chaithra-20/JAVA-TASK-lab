107679c0d82a8023d7a72c7a5c36e2a03601eda4
Chaithra-20 <chaithuvaishu20@gmail.com>
2022-07-11 19:55:13 +0530

    Third Push

--- 11-07-2022/LargestOfTwo.java
+++ 11-07-2022/LargestOfTwo.java
@@ -5,7 +5,7 @@ public static void main(String args[])
 {
 	Scanner ck=new Scanner(System.in);
 	System.out.print("Enter First value:");
-	int firstNumber=ck.netInt();
+	int firstNumber=ck.nextInt();
 	System.out.print("Enter Second value:");
 	int secondNumber=ck.nextInt();
 	if(firstNumber>secondNumber)
@@ -14,4 +14,6 @@ public static void main(String args[])
 	}
 	else{
 		System.out.println(secondNumber+" is largest");
+	}
+	}
 	}
